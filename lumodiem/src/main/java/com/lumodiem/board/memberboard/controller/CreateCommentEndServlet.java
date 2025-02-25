5c52ab624133bc2cd85ef1cdd997aa1db00f8556
tykkor22 <tykkor22@gmail.com>
2025-02-25 15:16:18 +0900

    [kty] memberboard => dao, sevice 패키지 추가

--- lumodiem/src/main/java/com/lumodiem/board/memberboard/controller/CreateCommentEndServlet.java
+++ lumodiem/src/main/java/com/lumodiem/board/memberboard/controller/CreateCommentEndServlet.java
@@ -2,24 +2,37 @@ package com.lumodiem.board.memberboard.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/createComment")
-public class CreateCommentServlet extends HttpServlet {
+import com.lumodiem.board.memberboard.vo.ReviewCmt;
+
+
+@WebServlet("/createcmt")
+public class CreateCommentEndServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public CreateCommentServlet() {
+    public CreateCommentEndServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher view = request.getRequestDispatcher("/views/comment/createComment.jsp");
-		view.forward(request, response);
+		String temp = request.getParameter("account_no");
+		String temp2 = request.getParameter("review_no");
+		String review_cmt_txt = request.getParameter("review_cmt_txt");
+		int account_no = 0;
+		int review_no = 0 ;
+		if(temp!=null) account_no = Integer.parseInt(temp);
+		if(temp2!=null) review_no = Integer.parseInt(temp2);
+		
+		ReviewCmt cmt = new ReviewCmt();
+		cmt.setAccountNo(account_no);
+		cmt.setReviewNo(review_no);
+		cmt.setReviewCmtTxt(review_cmt_txt);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
