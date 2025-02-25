f4beb0ff242ed1b2b88b79f3c0e130d7a02cc9e1
tykkor22 <tykkor22@gmail.com>
2025-02-25 12:41:50 +0900

    [kty] 관리자 일단 보류, 댓글 구현 시작

---
+++ lumodiem/src/main/java/com/lumodiem/board/adminboard/controller/ReportKlassSelect.java
@@ -0,0 +1,26 @@
+package com.lumodiem.board.adminboard.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/reportKlass")
+public class ReportKlassSelect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ReportKlassSelect() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
