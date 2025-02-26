f863aeffcccb9b5721a2a904e0b5acd87adf7671
devyej8753 <devyej8753@gmail.com>
2025-02-26 13:44:50 +0900

    [lyj] LumoDiem 리뷰게시판 수정중

--- lumodiem/src/main/java/com/lumodiem/board/memberboard/controller/UpdateReviewEndServlet.java
+++ lumodiem/src/main/java/com/lumodiem/board/memberboard/controller/UpdateReviewEndServlet.java
@@ -1,11 +1,15 @@
 package com.lumodiem.board.memberboard.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.lumodiem.board.memberboard.service.MemberBoardService;
+import com.lumodiem.board.memberboard.vo.Review;
 
 @WebServlet("/updateReviewEnd")
 public class UpdateReviewEndServlet extends HttpServlet {
@@ -16,11 +20,23 @@ public class UpdateReviewEndServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String temp = request.getParameter("board_no");
-		int 
+		String temp = request.getParameter("account_no");
+		int accountNo = 0;
+		if(temp!=null) accountNo = Integer.parseInt(temp);
+		String reviewName = request.getParameter("review_name");
+		String reviewTxt = request.getParameter("review_txt");
+		
+//		int resNo = 4;
+//		int klassDateNo = 2;
+		
+		Review review = Review.builder().reviewName(reviewName).reviewTxt(reviewTxt).build();
+		
+		int result = new MemberBoardService().UpdateReview(review);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
 		doGet(request, response);
 	}
 
