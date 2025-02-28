98a11ce7e17239724636eaa913c6f773bb326dec
devkskg <devkskg@gmail.com>
2025-02-28 11:57:14 +0900

    [ksk] host mypage 작업중2 2025-02-28
    	- 좋아요 목록 조회(작성중 mapper, mybatis-config 완료 -> jsp 작성중)
    	- approve_code 의 데이터 유형 제약조건을 VARCHAR -> ENUM(A,S,D) 로 변경

--- lumodiem/src/main/java/com/lumodiem/account/dao/HostMypageDao.java
+++ lumodiem/src/main/java/com/lumodiem/account/dao/HostMypageDao.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 
 import com.lumodiem.board.hostboard.vo.Klass;
+import com.lumodiem.board.hostboard.vo.KlassLike;
 import com.lumodiem.board.memberboard.vo.Reservation;
 import com.lumodiem.board.memberboard.vo.Review;
+import com.lumodiem.board.memberboard.vo.ReviewLike;
 
 public class HostMypageDao {
 
@@ -16,6 +18,14 @@ public class HostMypageDao {
 
 	public List<Review> selectReviewListByHostAccountNo(SqlSession session, Klass option) {
 		return session.selectList("mypageMapper.selectReviewListByHostAccountNo", option);
+	}
+
+	public List<KlassLike> selectKlassLikeByHostAccountNo(SqlSession session, Klass option) {
+		return session.selectList("mypageMapper.selectKlassLikeByHostAccountNo", option);
+	}
+
+	public List<ReviewLike> selectReviewLikeByHostAccountNo(SqlSession session, Klass option) {
+		return session.selectList("mypageMapper.selectReviewLikeByHostAccountNo", option);
 	}
 
 
