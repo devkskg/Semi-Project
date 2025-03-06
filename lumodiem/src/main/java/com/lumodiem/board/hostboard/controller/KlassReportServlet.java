dcdf10fedeace61f043a5509f4ba9506611511c9
862yuna <862yuna@gmail.com>
2025-03-06 17:59:26 +0900

    [cya] 클래스 상세조회 페이지에 신고버튼 추가
    	- bootstrap 3, 5 충돌로 bootstrap 5버전으로 코드 변경 (성관님)
    	- 아직은 확정 단계 전이라 주석 처리
    	- klass-mapper 에 KlassReportVo 추가 , mybatis-config 수정
    	- klassDetail.jsp 많이 수정하여 conflict 예상

--- lumodiem/src/main/java/com/lumodiem/board/hostboard/controller/KlassReportServlet.java
+++ lumodiem/src/main/java/com/lumodiem/board/hostboard/controller/KlassReportServlet.java
@@ -9,9 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.lumodiem.account.vo.Account;
+import org.json.simple.JSONObject;
+
 import com.lumodiem.board.hostboard.service.HostBoardService;
-import com.lumodiem.board.hostboard.vo.Klass;
+import com.lumodiem.board.hostboard.vo.KlassReport;
 
 @WebServlet("/klassReport")
 public class KlassReportServlet extends HttpServlet {
@@ -22,15 +23,42 @@ public class KlassReportServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String temp = request.getParameter("klass_no");
+		String temp1 = request.getParameter("klass_no");
+		String temp2 = request.getParameter("account");
+		int result = 0;
 		int klassNo = 0;
-		if(temp != null) klassNo = Integer.parseInt(temp);
-		Klass klass = new HostBoardService().selectKlassOne(klassNo);
+		int accountNo = 0;
+		
+		if(temp1 != null) klassNo = Integer.parseInt(temp1);
+		if(temp2 != null) accountNo = Integer.parseInt(temp2);
+		String reportKlassTxt = request.getParameter("report_klass_txt");
+		System.out.println("신고"+klassNo);
+		KlassReport klassReport = KlassReport.builder()
+								.accountNo(accountNo)
+								.klassNo(klassNo)
+								.reportKlassTxt(reportKlassTxt)
+								.build();
+		
+		result = new HostBoardService().insertKlassReport(klassReport);
+		
+		JSONObject obj = new JSONObject();
+		if(result > 0) {
+			obj.put("res_code", "200");
+			obj.put("res_msg", "정상적으로 신고가 완료되었습니다.");
+		}else {
+			obj.put("res_code", "500");
+			obj.put("res_msg", "게시글 신고중 오류가 발생하였습니다.");
+			
+		}
+		response.setContentType("application/json; charset=UTF-8");
+		response.getWriter().print(obj);
+		
+		
 		// 세션정보 확인해야함./
-		RequestDispatcher view = request.getRequestDispatcher("/views/klass/klassReport.jsp");
-		request.setAttribute("klass", klass);
-		System.out.println("신고서블릿 klass : "+klass);
-		view.forward(request, response);
+//		RequestDispatcher view = request.getRequestDispatcher("/views/klass/klassReport.jsp");
+//		request.setAttribute("klass", klass);
+//		System.out.println("신고서블릿 klass : "+klass);
+//		view.forward(request, response);
 	
 	}
 
